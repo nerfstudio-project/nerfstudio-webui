ee246d2f678d6382e0f2d359fbd769872eed7b48
Nicolas Zunhammer <n.zunhammer@dromni.eu>
2024-04-23 10:49:59 +0200

    Add argparser to webui.py

--- webui.py
+++ webui.py
@@ -1,5 +1,5 @@
 import gradio as gr
-
+import argparse
 from modules.data_processor_tab import DataProcessorTab
 from modules.exporter_tab import ExporterTab
 from modules.trainer_tab import TrainerTab
@@ -33,5 +33,22 @@ class WebUI:
 
 
 if __name__ == "__main__":
-    app = WebUI(root_dir="./", run_in_new_terminal=False)
-    app.launch(inbrowser=True, share=False)
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--root_dir', type=str, default='./',
+                        help='Root directory for data selection in web-ui')
+    parser.add_argument('--run_in_new_terminal', type=bool,
+                        default=False, help='Run commands in new terminal')
+    parser.add_argument('--share', type=bool, default=False,
+                        help='Create public gradio share link')
+    parser.add_argument('--server_name', type=str, default='0.0.0.0',
+                        help='IP address or hostname of the web-ui server')
+    parser.add_argument('--server_port', type=int,
+                        default=7860, help='Port of the web-ui server')
+
+    args = parser.parse_args()
+
+    app = WebUI(root_dir=args.root_dir,
+                run_in_new_terminal=args.run_in_new_terminal)
+    app.launch(inbrowser=True, share=args.share,
+               server_name=args.server_name, server_port=args.server_port)
